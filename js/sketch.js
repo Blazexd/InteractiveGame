9394f114911581643dc51f2af1efd5c46ffe5adb
Jack Cobb <72522046+Blazexd@users.noreply.github.com>
2022-11-15 12:02:48 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,43 @@
+var widthRect = 50;
+var heightRect = 50;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    background(220);
 }
 
 function draw() {
-    background(220);
+    
+    
+}
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+function mousePressed() {
+
+    if(mouseButton == LEFT){
+    fill(random(0, 255), random(0, 255), random(0, 255));
+    rectMode(CENTER);
+    rect(mouseX, mouseY, widthRect, heightRect);
+    }
+    
+    if (mouseButton == RIGHT){
+        fill(random(0, 255), random(0, 255), random(0, 255));
+        ellipseMode(CENTER);
+        ellipse(mouseX, mouseY, widthRect, heightRect);
+        }
+   
+}
+function keyPressed(){
+    if(keyCode == ENTER)
+    {background(0);
+    }
+
+if(keyCode == UP_ARROW){
+    fill(random(0, 255), random(0, 255), random(0, 255));
+    triangle(mouseX, mouseY, mouseX+50, mouseY, mouseX+75, mouseY-50);
+}
+if(keyCode == DOWN_ARROW){
+    fill(random(0, 255), random(0, 255), random(0, 255));
+    triangle(mouseX, mouseY, mouseX+50, mouseY, mouseX+20, mouseY-50);
+}
 }
